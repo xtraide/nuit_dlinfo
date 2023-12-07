f58480cfd4400cd412c480fe50c0e057368602b3
nicolas thieblemont <xxtraide@gmail.com>
2023-12-07 19:20:11 +0100

    switchCss

---
+++ js/script.js
@@ -0,0 +1,9 @@
+/* 
+    fonction pour switch des css 
+*/
+function switchCss() {
+    //togle une classe 
+    document.body.classList.toggle("first");
+
+    document.body.classList.toggle("second");
+}
