545781860c0ad81833692833ca1bbc1111d64dea
nicolas thieblemont <xxtraide@gmail.com>
2023-12-08 06:59:35 +0100

    link

--- Views/Template/Default.php
+++ Views/Template/Default.php
@@ -4,8 +4,8 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="/css/style.css">
-    <script defer src="js/script.js"></script>
+    <link rel="stylesheet" href="Views/Template/css/style.css">
+    <script defer src="Views/Template/js/script.js"></script>
 
     <title><?= App\App::getTitle();  ?></title>
 </head>
