21e7cdea832799dc4ff803161c6e7be5db949cba
Alina <alinaspicot@gmail.com>
2023-12-08 05:23:58 +0100

    probleme

--- Views/Template/js/function2.js
+++ Views/Template/js/function2.js
@@ -1,9 +1,10 @@
+//Ce truc marchait et ça marche plus
 function animateCountdown(startValue, endValue) {
-    let question = document.querySelector('.question');
     let reponse = document.querySelector('.reponse');
-    let bonne_reponse = "2013";
+    let bonne_reponse = "2050";
     let reponse_affiche = document.querySelector(".reponse_affiche");
     let number = 0;
+
     // Convertir les valeurs en nombres
     let startNumber = parseInt(startValue, 10);
     let endNumber = parseInt(endValue, 10);
@@ -12,7 +13,7 @@ function animateCountdown(startValue, endValue) {
     if (!isNaN(startNumber) && !isNaN(endNumber)) {
         // Déterminer le pas d'animation en fonction de la différence entre les deux valeurs
         let step = (startNumber > endNumber) ? -1 : 1;
-        
+
         // Démarrer l'animation avec une certaine intervalle
         let interval = setInterval(function() {
             // Mettre à jour la valeur du champ de texte
@@ -27,14 +28,17 @@ function animateCountdown(startValue, endValue) {
             // Arrêter l'animation lorsque la valeur atteint la bonne réponse
             if ((step === 1 && startNumber > endNumber) || (step === -1 && startNumber < endNumber)) {
                 clearInterval(interval);
-            }deuxième_question ()
-        }, 50); deuxième_question ()// Interval en millisecondes
-    }}
+                deuxième_question();
+            }
+        }, 50); // Interval en millisecondes
+    }
+}
+
 export function verifier_reponse() {
     const question = document.querySelector('.question');
     const reponse = document.querySelector('.reponse');
-    let bonne_reponse = "2013";
     const reponse_affiche = document.querySelector(".reponse_affiche");
+    let bonne_reponse = "2050";
     let number = 0;
 
     if (reponse.value === bonne_reponse) {
@@ -112,33 +116,107 @@ export function troisième_question_suivant () {
     }
     quatre ()
 }
-// function trois () {
-//     const suivant_un = document.querySelector(".suivant_deux");
-//     suivant_un.addEventListener("click", function () {
-//         document.querySelector(".one").style.display = "none";
-//         document.querySelector(".two").style.display = "none";
-//         document.querySelector(".three").style.display = "block";
-//         document.querySelector(".four").style.display = "none";
+function quatre () {
+    const suivant_un = document.querySelector(".suivant_trois");
+    suivant_un.addEventListener("click", function () {
+        document.querySelector(".one").style.display = "none";
+        document.querySelector(".two").style.display = "none";
+        document.querySelector(".three").style.display = "none";
+        document.querySelector(".four").style.display = "block";
+        document.querySelector(".five").style.display = "none";
 
-//         let bonne_reponse = "Heyy";
-//         const question = document.querySelector('.question2');
-//         const reponse3 = document.querySelector('.reponse_trois');
-//         const reponse_affiche3 = document.querySelector(".reponse_affiche_trois");
-//         let number = 0;
+        let bonne_reponse = "Heyy";
+        const question = document.querySelector('.question2');
+        const reponse3 = document.querySelector('.reponse_trois');
+        const reponse_affiche3 = document.querySelector(".reponse_affiche_deux");
+        let number = 0;
 
         
-//     })
+    })
     
-// }
-// //question 3
-// export function troisième_question_suivant () {
-//     const reponse3 = document.querySelector('.reponse_trois');
-//     let bonne_reponse = "Hello";
-//     const reponse_affiche3 = document.querySelector(".reponse_affiche_trois");
-//     if (reponse3.value === bonne_reponse) {
-//         console.log("Bonne réponse");
-//         reponse_affiche3.innerHTML = "<p> Bonne réponse3! <br>Votre réponse:" + reponse3.value + "</p>"  + "<button class='suivant_quatre'>suivant";
-//     } else {
-//         console.log("Essaie encore");
-//         reponse_affiche3.innerHTML = "<p> Mauvaise réponse3!! <br> Votre réponse:" + reponse3.value + "<br> la bonne réponse était:"+ bonne_reponse +"</p>" + "<button class='suivant_quatre'>suivant"
-//     }}
+}
+//question 4
+export function quatre_question_suivant () {
+    console.log("4")
+    const reponse4 = document.querySelector('.reponse_quatre');
+    let bonne_reponse = "Hello";
+    const reponse_affiche4 = document.querySelector(".reponse_affiche_quatre");
+    if (reponse4.value === bonne_reponse) {
+        console.log("Bonne réponse");
+        reponse_affiche4.innerHTML = "<p> Bonne réponse3! <br>Votre réponse:" + reponse4.value + "</p>"  + "<button class='suivant_quatre'>suivant";
+    } else {
+        console.log("Essaie encore");
+        reponse_affiche4.innerHTML = "<p> Mauvaise réponse3!! <br> Votre réponse:" + reponse4.value + "<br> la bonne réponse était:"+ bonne_reponse +"</p>" + "<button class='suivant_quatre'>suivant"
+    }
+    cinq ()
+}
+function cinq () {
+    const suivant_un = document.querySelector(".suivant_quatre");
+    suivant_un.addEventListener("click", function () {
+        document.querySelector(".one").style.display = "none";
+        document.querySelector(".two").style.display = "none";
+        document.querySelector(".three").style.display = "none";
+        document.querySelector(".four").style.display = "none";
+        document.querySelector(".five").style.display = "block";
+
+        let bonne_reponse = "Heyy";
+        const question = document.querySelector('.question2');
+        const reponse3 = document.querySelector('.reponse_trois');
+        const reponse_affiche3 = document.querySelector(".reponse_affiche_deux");
+        let number = 0;
+
+        
+    })
+    
+}
+//question 5
+export function cinq_question_suivant () {
+    console.log("5")
+    const reponse5 = document.querySelector('.reponse_cinq');
+    let bonne_reponse = "Hello";
+    const reponse_affiche5 = document.querySelector(".reponse_affiche_cinq");
+    if (reponse5.value === bonne_reponse) {
+        console.log("Bonne réponse");
+reponse_affiche5.innerHTML = "<p> Bonne réponse3! <br>Votre réponse:" + reponse5.value + "</p>" + "<button class='suivant_cinq'>suivant";
+    } else {
+        console.log("Essaie encore");
+        reponse_affiche5.innerHTML = "<p> Mauvaise réponse3!! <br> Votre réponse:" + reponse5.value + "<br> la bonne réponse était:"+ bonne_reponse +"</p>" + "<button class='suivant_cinq'>suivant"
+    }
+    six ()
+}
+
+function six () {
+    const suivant_un = document.querySelector(".suivant_cinq");
+    suivant_un.addEventListener("click", function () {
+        document.querySelector(".one").style.display = "none";
+        document.querySelector(".two").style.display = "none";
+        document.querySelector(".three").style.display = "none";
+        document.querySelector(".four").style.display = "none";
+        document.querySelector(".five").style.display = "none";
+        document.querySelector(".six").style.display = "block";
+
+        let bonne_reponse = "Heyy";
+        const question = document.querySelector('.question2');
+        const reponse3 = document.querySelector('.reponse_trois');
+        const reponse_affiche3 = document.querySelector(".reponse_affiche_deux");
+        let number = 0;
+
+        
+    })
+    
+}
+//question 6
+export function six_question_suivant () {
+    console.log("6")
+    const reponse6 = document.querySelector('.reponse_six');
+    let bonne_reponse = "Hello";
+    const reponse_affiche6 = document.querySelector(".reponse_affiche_six");
+    if (reponse6.value === bonne_reponse) {
+        console.log("Bonne réponse");
+reponse_affiche6.innerHTML = "<p> Bonne réponse3! <br>Votre réponse:" + reponse6.value + "</p>" + "<button class='suivant_cinq'>suivant";
+    } else {
+        console.log("Essaie encore");
+        reponse_affiche6.innerHTML = "<p> Mauvaise réponse3!! <br> Votre réponse:" + reponse6.value + "<br> la bonne réponse était:"+ bonne_reponse +"</p>" + "<button class='suivant_cinq'>suivant"
+    }
+    six ()
+}
