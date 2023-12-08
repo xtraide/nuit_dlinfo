890c1eb6e42537c80553db573ce6546995a8fe0e
Raphaël <115149179+rboucheron@users.noreply.github.com>
2023-12-08 03:20:29 +0100

    cc

---
+++ Views/Template/js/musicNoël.js
@@ -0,0 +1,13 @@
+
+var MusicIsPlay = false;
+var audio = document.getElementById('myAudio'); 
+function play() {
+    if (MusicIsPlay === false) {
+        audio.play(); 
+        MusicIsPlay = true;
+    } else {
+        audio.pause();
+        MusicIsPlay = false;
+    }
+}
+
