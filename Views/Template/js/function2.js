e9289841cd35b6eafaf996d4e431bf12e7d3c6cf
Alina <alinaspicot@gmail.com>
2023-12-08 01:23:42 +0100

    Kahoot (code sale)

--- Views/Template/js/function2.js
+++ Views/Template/js/function2.js
@@ -14,7 +14,8 @@ export function verifier_reponse() {
     }
     deuxième_question ()
 }
-function deuxième_question () {
+//question 1
+ function deuxième_question () {
     const suivant_un = document.querySelector(".suivant_un");
     suivant_un.addEventListener("click", function () {
         document.querySelector(".one").style.display = "none";
@@ -27,15 +28,84 @@ function deuxième_question () {
         const reponse2 = document.querySelector('.reponse_deux');
         const reponse_affiche2 = document.querySelector(".reponse_affiche_deux");
         let number = 0;
+        
     })
-    function deuxième_question_suivant () {
-        if (reponse2.value === bonne_reponse) {
-            console.log("Bonne réponse");
-            reponse_affiche2.innerHTML = "<p> Bonne réponse2! <br>Votre réponse:" + reponse2.value + "</p>"  + "<button class='suivant_un'>suivant";
-        } else {
-            console.log("Essaie encore");
-            reponse_affiche2.innerHTML = "<p> Mauvaise réponse2!! <br> Votre réponse:" + reponse2.value + "<br> la bonne réponse était:"+ bonne_reponse +"</p>" + "<button class='suivant_un'>suivant"
-        }
-    }
     
 }
+//question 2
+export function deuxième_question_suivant () {
+    const reponse2 = document.querySelector('.reponse_deux');
+    let bonne_reponse = "Hello";
+    const reponse_affiche2 = document.querySelector(".reponse_affiche_deux");
+    if (reponse2.value === bonne_reponse) {
+        console.log("Bonne réponse");
+        reponse_affiche2.innerHTML = "<p> Bonne réponse2! <br>Votre réponse:" + reponse2.value + "</p>"  + "<button class='suivant_deux'>suivant";
+    } else {
+        console.log("Essaie encore");
+        reponse_affiche2.innerHTML = "<p> Mauvaise réponse2!! <br> Votre réponse:" + reponse2.value + "<br> la bonne réponse était:"+ bonne_reponse +"</p>" + "<button class='suivant_deux'>suivant"
+    }
+    trois ()
+}
+function trois () {
+    const suivant_un = document.querySelector(".suivant_deux");
+    suivant_un.addEventListener("click", function () {
+        document.querySelector(".one").style.display = "none";
+        document.querySelector(".two").style.display = "none";
+        document.querySelector(".three").style.display = "block";
+        document.querySelector(".four").style.display = "none";
+
+        let bonne_reponse = "Heyy";
+        const question = document.querySelector('.question2');
+        const reponse3 = document.querySelector('.reponse_trois');
+        const reponse_affiche3 = document.querySelector(".reponse_affiche_deux");
+        let number = 0;
+
+        
+    })
+    
+}
+//question 3
+export function troisième_question_suivant () {
+    console.log("3")
+    const reponse3 = document.querySelector('.reponse_trois');
+    let bonne_reponse = "Hello";
+    const reponse_affiche3 = document.querySelector(".reponse_affiche_trois");
+    if (reponse3.value === bonne_reponse) {
+        console.log("Bonne réponse");
+        reponse_affiche3.innerHTML = "<p> Bonne réponse3! <br>Votre réponse:" + reponse3.value + "</p>"  + "<button class='suivant_trois'>suivant";
+    } else {
+        console.log("Essaie encore");
+        reponse_affiche3.innerHTML = "<p> Mauvaise réponse3!! <br> Votre réponse:" + reponse3.value + "<br> la bonne réponse était:"+ bonne_reponse +"</p>" + "<button class='suivant_trois'>suivant"
+    }
+    quatre ()
+}
+// function trois () {
+//     const suivant_un = document.querySelector(".suivant_deux");
+//     suivant_un.addEventListener("click", function () {
+//         document.querySelector(".one").style.display = "none";
+//         document.querySelector(".two").style.display = "none";
+//         document.querySelector(".three").style.display = "block";
+//         document.querySelector(".four").style.display = "none";
+
+//         let bonne_reponse = "Heyy";
+//         const question = document.querySelector('.question2');
+//         const reponse3 = document.querySelector('.reponse_trois');
+//         const reponse_affiche3 = document.querySelector(".reponse_affiche_trois");
+//         let number = 0;
+
+        
+//     })
+    
+// }
+// //question 3
+// export function troisième_question_suivant () {
+//     const reponse3 = document.querySelector('.reponse_trois');
+//     let bonne_reponse = "Hello";
+//     const reponse_affiche3 = document.querySelector(".reponse_affiche_trois");
+//     if (reponse3.value === bonne_reponse) {
+//         console.log("Bonne réponse");
+//         reponse_affiche3.innerHTML = "<p> Bonne réponse3! <br>Votre réponse:" + reponse3.value + "</p>"  + "<button class='suivant_quatre'>suivant";
+//     } else {
+//         console.log("Essaie encore");
+//         reponse_affiche3.innerHTML = "<p> Mauvaise réponse3!! <br> Votre réponse:" + reponse3.value + "<br> la bonne réponse était:"+ bonne_reponse +"</p>" + "<button class='suivant_quatre'>suivant"
+//     }}
--- Views/Template/js/kahoot.js
+++ Views/Template/js/kahoot.js
@@ -1,4 +1,10 @@
-import {verifier_reponse} from './function2.js';
+import {verifier_reponse, deuxième_question_suivant, troisième_question_suivant} from './function2.js';
 const valider = document.querySelector('.valider');
 
 valider.addEventListener('click', verifier_reponse);
+
+const valider2 = document.querySelector('.valider_deux');
+valider2.addEventListener('click', deuxième_question_suivant);
+
+const valider3 = document.querySelector('.valider_trois');
+valider3.addEventListener('click', troisième_question_suivant);
