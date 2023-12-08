c96d2742cdceb202b16ab78e9c4b28dbadf254ed
nicolas thieblemont <xxtraide@gmail.com>
2023-12-08 03:58:03 +0100

    bonne requete

--- Views/Page/section_1.php
+++ Views/Page/section_1.php
@@ -4,3 +4,9 @@
     dolorum ea voluptate praesentium repudiandae tempora reiciendis nesciunt voluptas modi doloremque non. Quod quisquam
     numquam quis ipsa, laboriosam exercitationem molestiae saepe nesciunt.
 </h1>
+
+<?php
+$app = App\App::getInstance();
+
+$post = $app->getTable('Compte');
+var_dump($post->all()); ?>
