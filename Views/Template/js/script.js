7b9694923db365679b954f24e1fae45eb6829675
RETR0EDIT <nolandme2003@gmail.com>
2023-12-08 02:16:51 +0100

    fonction fake news

--- Views/Template/js/script.js
+++ Views/Template/js/script.js
@@ -1,7 +1,7 @@
-/* 
-    fonction pour switch des css (defi theme n°392)
-*/
-function defi_theme() {
+
+   // fonction pour switch des css (defi theme n°392)
+
+   function defi_theme() {
     //togle une classe 
     document.body.classList.toggle("first");
 
@@ -23,3 +23,59 @@ document.addEventListener('DOMContentLoaded', function () {
 
   });
 });
+
+document.addEventListener('DOMContentLoaded', function () {
+
+  var pointerZone = document.getElementById('pointer-zone');
+
+
+  var toggleState = false;
+
+
+  document.addEventListener('keydown', function (event) {
+
+    if (event.key === 'c' || event.key === 'C') {
+
+      toggleState = !toggleState;
+
+
+      if (toggleState) {
+        pointerZone.style.display = 'block';
+        
+      } else {
+        pointerZone.style.display = 'none';
+      }
+    }
+  });
+});
+function toggleCouleurFond(id) {
+  var divQR = document.getElementById(id);
+
+  // Vérifie si la couleur actuelle est bleue
+  if (divQR.style.backgroundColor === '') {
+    // Si oui, change la couleur en vert
+    divQR.style.backgroundColor = 'green';
+  } else {
+    // Sinon, remet la couleur en bleu
+    divQR.style.backgroundColor = '';
+  }
+}
+
+function valider() {
+  // Ajoutez votre logique de validation ici
+  alert("Validation réussie !");
+}
+const txt = document.getElementById('switch')
+const none = document.getElementById('none')
+
+function switch1() {
+  if(txt.style.display === 'block'){
+    txt.style.display = 'none';
+    none.style.display = 'block';
+  }
+  else{
+    txt.style.display = 'block';
+    none.style.display = 'none';
+    console.log('gggg')
+  }
+}
