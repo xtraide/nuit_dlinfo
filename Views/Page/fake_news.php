45e7615f0c2e25c1d56d631afc265d0d43ad63ac
Anchana <fatimarajananchana@gmail.com>
2023-12-08 05:54:10 +0100

    dfgyjui_

--- Views/Page/fake_news.php
+++ Views/Page/fake_news.php
@@ -7,3 +7,40 @@
   <!-- vrai article-->
   <div class="article" id="div2">phrase qui doit aparaitre qd c rouge</div>
 </div>
+
+<link rel="stylesheet" href="../Template/css/style.css">
+
+<body class="fake_body">
+  <div class="special special2">
+    <p>Article 2</p>
+    <div class="hr"></div>
+  </div>
+
+  <h1 class="titre_principal titre_principal2 titre_principal_mobile">2023, l'année "la plus chaude jamais observée"
+  </h1>
+  <h1 class="titre_principal titre_principal_desk">L'urgence climatique, et nous ?</h1>
+  <section class="chapeau"><br>
+    <p>Les champs'ions du meaux'de avion</p>
+    <p>08 Décembre 2023, 8h</p> <br>
+    <div class="hr hr2"></div>
+    </div>
+    <div class="container">
+
+      <div class="container_intro">
+        <div class="text_intro">
+          <h2 class="petite_intro">Petite intro</h2>
+          <p>Les cyclones dévastateurs, les montées des eaux menaçantes et les méga-feux sont
+            autant de manifestations alarmantes de la dégradation climatique qui occupent quotidiennement nos
+            conversations. Cependant, une réalité souvent méconnue émerge de ce tableau inquiétant : la
+            migration écologique est en train de devenir la première forme de déplacement à l'échelle
+            internationale. De plus en plus, des communautés entières se voient contraintes de migrer afin
+            d'échapper au tragique destin qui les guette dans des territoires condamnés par les ravages du
+            changement climatique.</p>
+        </div>
+        <div class="image_intro"> <img src="../Template/assets/img/image-evolutions.png" alt="">
+        </div>
+
+      </div>
+    </div>
+
+</body>
